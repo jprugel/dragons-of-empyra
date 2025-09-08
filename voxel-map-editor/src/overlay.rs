a1a71b5a9bc4011c982139009bdb33d2482e28d8
jprugel <jonathon.prugel@gmail.com>
2025-09-08 07:54:22 -0400

    Beginning rebuilding overlay for voxel-map-editor

---
+++ voxel-map-editor/src/overlay.rs
@@ -0,0 +1,32 @@
+use bevy::prelude::*;
+
+struct OverlayPlugin;
+
+impl Plugin for OverlayPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, overlay_system);
+    }
+}
+
+/// This system will be an overlay similar to the one in magicka voxel editor.
+/// Things we need:
+/// - Map Dimension Input
+/// - Tile Dimension Input
+/// - Navbar for importing tiles or old maps.
+fn setup_overlay(mut commands: Commands) {
+    let canvas_node = Node::builder()
+        .width(Val::Percent(100.0))
+        .height(Val::Percent(100.0))
+        .build();
+
+    let bar_node = Node::builder()
+        .width(Val::Percent(100.0))
+        .height(Val::Percent(5.0))
+        .build();
+
+    let canvas_bundle = (canvas_node);
+
+    let canvas = commands.spawn(canvas_node).id();
+
+    let bar = commands.spawn(bar_node).insert(ChildOf(canvas)).id();
+}
